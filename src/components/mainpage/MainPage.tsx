56bb77500dd3888390b65d722fbea3c748bd9597
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-05-14 15:16:21 -0300

    Refactor: MainPage es un afuncion ahora
        refs:#3

--- src/components/mainpage/MainPage.tsx
+++ src/components/mainpage/MainPage.tsx
@@ -1,44 +1,21 @@
 import * as React from 'react';
-import {connect} from 'react-redux';
 
 import './MainPage.css';
 
 import SearchBox from '../common/searchbox/SearchBox'
-import Serie from '../common/serie/Serie'
 import Featured from './featured/Featured'
 
-interface IMainPageState {
+export default (() => {
+    return (
+        <div className='MainPage'>
+            <h1>Series de tiempo</h1>
+            <p>Desde aquí poedés buscar las series de tiempo del tema que necesites,
+                 podés seleccionar más de una opcion de los resultados para compararlos.</p>
+            <SearchBox />
 
-    featured: Serie[]
+            <Featured />
+
+        </div>
+    );
 }
-
-class MainPage extends React.Component<any, IMainPageState> {
-
-    constructor(props: any, context: any){
-        super(props, context);
-
-        this.state = {featured: Object.assign({}, this.props.featured )};
-    }
-
-    public render () {
-        return (
-            <div className='MainPage'>
-                <h1>Series de tiempo</h1>
-                <p>Desde aquí poedés buscar las series de tiempo del tema que necesites,
-                     podés seleccionar más de una opcion de los resultados para compararlos.</p>
-                <SearchBox />
-
-                <Featured />
-                
-            </div>
-        );
-    }
-}
-
-function mapStateToProps(state: any, ownProps: any): IMainPageState{
-    return {
-        featured: state.featured
-    };
-}
-
-export default connect(mapStateToProps)(MainPage);
+);
