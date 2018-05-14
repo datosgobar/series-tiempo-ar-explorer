2c487aacdc9af73c6b41c1bbf5780ee0050a3b7a
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-05-14 10:17:20 -0300

    Agrego archivo de configuracion de series destacadas
        refs:#3

--- src/components/mainpage/featured/FeaturedReducer.tsx
+++ src/components/mainpage/featured/FeaturedReducer.tsx
@@ -1,11 +1,6 @@
-import * as React from 'react';
-import Serie from '../../common/serie/Serie'
-// TODO: reemplazar default por lista vacia, esto es solo una prueba
-const series = [
-    <Serie key={1} name='PBI General Mensual' author='Subsecretaria de Programación Macroeconomica' description='Exportaciones por provincia y por pais de destino en millones de dolares' />,
-    <Serie key={2} name='2PBI General Mensual' author='Subsecretaria de Programación Macroeconomica' description='Exportaciones por provincia y por pais de destino en millones de dolares' />
-];
+import featured from "../../../conf/featured";
+import Serie from "../../common/serie/Serie";
 
-export default function FeaturedReducer(state: any = series, action: any) {
-    return state;
+export default function featuredReducer(state: Serie[] = [], action: any): Serie[] {
+    return state.concat(featured);
 }
---
+++ src/conf/featured.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Serie from "../components/common/serie/Serie";
+
+const featured: any[] = [
+    <Serie key={1}  name='PBI General Mensual' 
+                    author='Subsecretaria de Programación Macroeconomica' 
+                    description='Exportaciones por provincia y por pais de destino en millones de dolares' />,
+    <Serie key={2}  name='2PBI General Mensual'
+                    author='Subsecretaria de Programación Macroeconomica' 
+                    description='Exportaciones por provincia y por pais de destino en millones de dolares' />
+];
+
+export default featured;
