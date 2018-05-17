48ae59dcd323d0a30ddac72585a4dfac8e38b42f
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-05-17 14:57:00 -0300

    Agrego test de comportamiento en searchbox
        refs:#3

--- src/components/common/searchbox/SearchBox.test.tsx
+++ src/components/common/searchbox/SearchBox.test.tsx
@@ -2,18 +2,59 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
-import configureStore from '../../../store/configureStore';
-import SearchBox from './SearchBox';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  const store = configureStore();
-  ReactDOM.render(
-    <MemoryRouter>
-      <Provider store={store}>
-        <SearchBox />
-      </Provider>
-    </MemoryRouter>
-    , div);
-  ReactDOM.unmountComponentAtNode(div);
+import configureStore from '../../../store/configureStore';
+import { SearchBox } from './SearchBox';
+
+
+import { configure, mount } from 'enzyme'
+import * as Adapter from 'enzyme-adapter-react-16';
+
+
+configure({ adapter: new Adapter() });
+
+
+describe('searchbox', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    const store = configureStore();
+    ReactDOM.render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <SearchBox />
+        </Provider>
+      </MemoryRouter>
+      , div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('redirects to search page when submited', () => {
+
+    const historyMock = {
+      push: jest.fn()
+    };
+
+    const wrapper = mount(<SearchBox history={historyMock}/>);
+
+    wrapper.find('.SearchBox').find('input').last().simulate('click');
+
+    expect(historyMock.push).toBeCalledWith('/search/');
+  });
+
+  it('redirects to search page when submited with searchterm', () => {
+
+    const searchTerm = 'searchTerm'
+
+    const historyMock = {
+      push: jest.fn()
+    };
+
+    const wrapper = mount(<SearchBox history={historyMock}/>);
+
+    wrapper.find('.SearchBox').find('input').first().simulate('change', { target: { value: searchTerm } });
+
+    wrapper.find('.SearchBox').find('input').last().simulate('click');
+
+    expect(historyMock.push).toBeCalledWith('/search/' + searchTerm);
+  });
 });
