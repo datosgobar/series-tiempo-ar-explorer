20dc47f8198325d5e5295fe47483faf6605fdcfb
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-05-22 10:04:35 -0300

    Agrego export a los graficos
        refs:#6

--- src/components/viewpage/graphic/Graphic.tsx
+++ src/components/viewpage/graphic/Graphic.tsx
@@ -3,6 +3,19 @@ import * as React from 'react';
 // tslint:disable-next-line:no-var-requires
 const ReactHighcharts = require('react-highcharts');
 
+// Highcharts more
+// tslint:disable-next-line:no-var-requires
+require('highcharts-more')(ReactHighcharts.Highcharts);
+
+// Highcharts exporting
+// tslint:disable-next-line:no-var-requires
+require('highcharts-exporting')(ReactHighcharts.Highcharts);
+
+// Highcharts export csv
+// tslint:disable-next-line:no-var-requires
+require('highcharts-export-csv')(ReactHighcharts.Highcharts)
+
+
 import IDataPoint from '../../../api/DataPoint';
 import { ISerie } from '../../../api/Serie';
 
@@ -15,18 +28,40 @@ interface IGraphicProps {
 
 export class Graphic extends React.Component<IGraphicProps, any> {
 
+    get categories() {
+        return (
+            this.props.series.map(
+                (serie: ISerie) => serie.data.map(
+                    (datapoint: IDataPoint) => datapoint.date))
+            [0]
+            || []
+        );
+    }
+
+    get seriesValues() {
+        return (
+            this.props.series.map(
+                (serie: ISerie) => ({
+                    data: serie.data.map((datapoint: IDataPoint) => datapoint.value),
+                    name: serie.id,
+                })
+            )
+        );
+    }
+
     get highchartsConfig() {
         return ({
+
             title: {
                 text: 'Chart reflow is set to true'
             },
 
             xAxis: {
-                categories: this.props.series.map((serie: ISerie) => serie.data.map((datapoint: IDataPoint) => datapoint.date))[0] || [] ,
+                categories: this.categories,
             },
 
-            series: this.props.series.map(serie => ({ name: serie.id, data: serie.data.map((datapoint: IDataPoint) => datapoint.value) })),
-            
+            series: this.seriesValues,
+
             subtitle: {
                 text: 'When resizing the window or the frame, the chart should resize'
             },
