2b3907541caa5d60393440df59794f3f1d08bb67
Leandro Gomez <lgomez@devartis.com>
2018-05-23 09:38:45 -0300

    Uso el componente Link para el ruteo a la vista de una serie

--- src/components/common/serie/Serie.tsx
+++ src/components/common/serie/Serie.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import './Serie.css';
 
-import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import { ISerie } from '../../../api/Serie';
 
 interface ISerieProps extends RouteComponentProps<any>{
@@ -11,25 +11,13 @@ interface ISerieProps extends RouteComponentProps<any>{
 
 class Serie extends React.Component<ISerieProps, any> {
 
-    constructor(props: ISerieProps, context: any){
-        super(props, context);
-
-        this.viewSerie = this.viewSerie.bind(this);
-    }
-
-    public viewSerie(event: any) {
-        const uri = '/view/?id=' + this.props.serie.id;
-
-        this.props.history.push(uri);
-    }
-
     public render () {
         return (
-            <div className='Serie' onClick={this.viewSerie}>
+            <Link className='Serie' to={`/view/?id=${this.props.serie.id}`}>
                 <h5>{this.props.serie.title}</h5>
                 <h6>{this.props.serie.publisher.name}</h6>
                 <p>{this.props.serie.description}</p>
-            </div>
+            </Link>
         );
     }
 }
