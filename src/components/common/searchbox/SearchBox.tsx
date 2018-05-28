b1bdc78438c2166d5539bd931f31ad9b2eed25c1
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-05-28 16:53:05 -0300

    Fix: ahora el searchbox dispara la busqueda al dar enter
        refs:#5

--- src/components/common/searchbox/SearchBox.tsx
+++ src/components/common/searchbox/SearchBox.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect, withRouter } from 'react-router';
 
 import './SearchBox.css';
 
 interface ISearchBoxState {
 
     searchTerm: string;
+    redirect: JSX.Element | null;
 }
 
 export class SearchBox extends React.Component<any, ISearchBoxState> {
@@ -13,9 +14,10 @@ export class SearchBox extends React.Component<any, ISearchBoxState> {
     constructor(props: any) {
         super(props);
 
-        this.state = { searchTerm: "" };
+        this.state = { searchTerm: "", redirect: null };
 
         this.onSearchTermChange = this.onSearchTermChange.bind(this);
+        this.onSearch = this.onSearch.bind(this);
     }
 
     public onSearchTermChange(event: any) {
@@ -23,24 +25,34 @@ export class SearchBox extends React.Component<any, ISearchBoxState> {
         this.setState({ searchTerm: newSearchTerm });
     }
 
+    public componentDidUpdate(){
+        if(this.state.redirect){
+            this.setState({redirect: null});
+        }
+    }
+
+    public onSearch(event: any) {
+
+        this.setState({ redirect: <Redirect to={`/search/?q=${this.state.searchTerm}`} /> });
+    }
 
     public render() {
         return (
             <div className='SearchBox'>
+                {this.state.redirect}
+                <form onSubmit={this.onSearch} >
+                    <input
+                        type='text'
+                        placeholder='Buscar Serie'
+                        onChange={this.onSearchTermChange} />
 
-                <input
-                    type='text'
-                    placeholder='Buscar Serie'
-                    onChange={this.onSearchTermChange} />
-
-                <Link to={`/search/?q=${this.state.searchTerm}`}>
                     <input
                         type='submit'
-                        value='Buscar'/>
-                </Link>
+                        value='Buscar' />
+                </form>
             </div>
         );
     }
 }
 
-export default SearchBox;
+export default withRouter(SearchBox);
