2f191b29ba8d5cac39c86c9b8a44bbd6da36473f
Leandro Gomez <lgomez@devartis.com>
2018-05-29 16:51:24 -0300

    Permito generar multiples series desde un array de IDs

--- src/tests/support/factories/series_api.ts
+++ src/tests/support/factories/series_api.ts
@@ -1,54 +1,60 @@
 import ITSAPIResponse from "../../../api/ITSAPIResponse";
 
 
-export function generateITSAPIResponse(): ITSAPIResponse {
+export function generateITSAPIResponse(tsIDs: string[] = ["1.1", "1.2"]): ITSAPIResponse {
 
     const data = [
         [
             "2017-10-01",
-            128.1
         ],
         [
             "2017-11-01",
-            125.9
         ]
-    ];
+    ].map((singleData) => {
+        return [singleData, ...tsIDs.map(tsID => {
+            tsIDs.indexOf(tsID)
+        })];
+    });
+
     const meta = [
-            {
-                "dataset": []
-            },
-            {
+        {
+            "dataset": []
+        },
+        ...tsIDs.map((tsID) => {
+            return {
                 "dataset": [
                     {
                         "distribution": [
                             {
                                 "field": [
                                     {
-                                        "description": "example field description",
-                                        "id": "example field id",
-                                        "title": "example field title",
+                                        "description": `${tsID} field description`,
+                                        "id": tsID,
+                                        "title": `${tsID} field title`,
                                     }
                                 ],
                             }
                         ],
                         "publisher": {
-                            "mbox": "example@dataset.com",
-                            "name": "example dataset publisher name"
+                            "mbox": `${tsID}@dataset.com`,
+                            "name": `${tsID} dataset publisher name`
                         },
                     }
                 ],
             }
-        ];
+        }),
+
+    ];
 
     const params = {
-        "identifiers": [
-            {
+        "identifiers": tsIDs.map(tsID => {
+            return {
                 "dataset": "1",
                 "distribution": "1.1",
-                "id": "example field id"
+                "id": tsID
             }
-        ],
-        "ids": "example field id",
+        }),
+        "ids": tsIDs.join(","),
         "metadata": "full"
     };
 
