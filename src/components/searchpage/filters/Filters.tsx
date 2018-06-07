5bd8d81caf96eff5555891a4db7c1dffa110f59d
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-06-07 09:17:43 -0300

    Agrego FilterThemes
        refs:#19

--- src/components/searchpage/filters/Filters.tsx
+++ src/components/searchpage/filters/Filters.tsx
@@ -2,17 +2,22 @@ import * as React from 'react';
 
 import { ISerieApi } from '../../../api/SerieApi';
 import FilterSources from './filtersources/FilterSources';
+import FilterThemes from './filterthemes/FilterThemes';
 
 interface IFilterProps {
 
     seriesApi: ISerieApi;
-    onSourcePicked: (event: React.MouseEvent<HTMLElement>, source:string) => void;
+    onSourcePicked: (event: React.MouseEvent<HTMLElement>, source: string) => void;
+    onThemePicked: (event: React.MouseEvent<HTMLElement>, theme: string) => void;
 }
 
 function Filters(props: IFilterProps) {
 
     return (
-        <FilterSources seriesApi={props.seriesApi} onSourcePicked={props.onSourcePicked}/>
+        <div>
+            <FilterSources seriesApi={props.seriesApi} onSourcePicked={props.onSourcePicked} />
+            <FilterThemes seriesApi={props.seriesApi} onThemePicked={props.onThemePicked} />
+        </div>
     );
 };
 
---
+++ src/components/searchpage/filters/filterthemes/FilterThemes.tsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+import { connect } from "react-redux";
+
+import { ISerieApi } from "../../../../api/SerieApi";
+import { IStore } from "../../../../store/initialState";
+
+interface IFilterThemesProps {
+
+    seriesApi: ISerieApi;
+    onThemePicked: (event: React.SyntheticEvent<HTMLElement>, source: string) => void;
+    picked: string;
+}
+
+interface IFilterThemesState {
+
+    themes: string[];
+}
+
+export class FilterThemes extends React.Component<IFilterThemesProps, IFilterThemesState> {
+
+    constructor(props: IFilterThemesProps) {
+        super(props);
+
+        this.state = { themes: [] };
+
+        this.handleClick = this.handleClick.bind(this);
+        this.toCheckBox = this.toCheckBox.bind(this);
+    }
+
+    public updateThemes() {
+        this.props.seriesApi.fetchThemes().then(themes => {
+            this.setState({ themes });
+        });
+    }
+
+    public componentDidMount() {
+        this.updateThemes()
+    }
+
+    public handleClick(source: string) {
+        return (event: React.SyntheticEvent<HTMLElement>) => {
+            this.props.onThemePicked(event, source);
+        };
+    }
+
+    public render() {
+        return (
+            <div>
+                <h3>Themes</h3>
+                <div>
+                    {this.state.themes.map(this.toCheckBox)}
+                </div>
+            </div>
+        );
+    }
+
+    public toCheckBox(source: string) {
+        return (
+            <div className="Theme" key={source}>
+                <label>
+                    <input type="radio" value={source} checked={this.props.picked === source} onChange={this.handleClick(source)} />
+                    {source}
+                </label>
+            </div>);
+    }
+}
+
+function mapStateToProps(state: IStore) {
+    return {
+        picked: state.searchParams.theme,
+    };
+}
+
+export default connect(mapStateToProps)(FilterThemes);
