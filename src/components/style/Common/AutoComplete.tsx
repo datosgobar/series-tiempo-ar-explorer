1e4b87c8049b278c64a09f94bef63a429ed2a391
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-06-19 14:41:37 -0300

    Fix: arreglo estilo de searchbox autocomplete
        refs:#31

--- src/components/style/Common/AutoComplete.tsx
+++ src/components/style/Common/AutoComplete.tsx
@@ -4,4 +4,4 @@ import * as React from 'react';
 
 export default (props: AutoComplete.Props) =>
 
-    <AutoComplete inputProps={{ className: "form-control" }} {...props} />
+    <AutoComplete inputProps={{ className: "form-control" }} wrapperStyle={{}} {...props} />
