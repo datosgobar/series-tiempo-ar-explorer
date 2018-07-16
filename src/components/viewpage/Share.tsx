e41fa509f1b026f22b3bd1608180772a51923533
Alejandro Sobko <alee-sk8@hotmail.com>
2018-07-16 12:06:48 -0300

    Agrego copiar al portapapeles y fix para compartir en twitter
        refs:#55

--- src/components/viewpage/Share.tsx
+++ src/components/viewpage/Share.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import * as CopyToClipboard from 'react-copy-to-clipboard';
+import * as ReactTooltip from 'react-tooltip'
 import {ISerie} from "../../api/Serie";
 
 interface IShareProps {
@@ -10,6 +12,16 @@ export class Share extends React.Component<IShareProps, any> {
 
     constructor(props: IShareProps) {
         super(props);
+        this.onCopy = this.onCopy.bind(this);
+        this.copyMessage = this.copyMessage.bind(this);
+    }
+
+    public componentWillMount() {
+        this.setState({copied: false, url: this.props.url});
+    }
+
+    public onCopy() {
+        this.setState({copied: true, url: window.location.href})
     }
 
     public render() {
@@ -20,18 +32,25 @@ export class Share extends React.Component<IShareProps, any> {
                     <li><a href={this.tweetMessage()} target="_blank"><i className="fab fa-twitter" /></a></li>&nbsp;
                     <li><a><i className="fab fa-facebook-f" /></a></li>
                 </ul>
-                <span className="url">
-                    <i className="fa fa-link" />&nbsp;
-                    <span className="url-text">Enlace para compartir: <a href={this.props.url}>{this.props.url}</a></span>
-                </span>
+                <CopyToClipboard text={this.props.url} onCopy={this.onCopy}>
+                    <span className="url" style={{cursor: 'pointer'}} data-tip={this.copyMessage()} >
+                        <i className="fa fa-link" />&nbsp;
+                        <span className="url-text">Enlace para compartir: <span className="legend">{this.props.url}</span></span>
+                    </span>
+                </CopyToClipboard>
                 <span className="legend">Guarda este link para automatizar la búsqueda</span>
+                <ReactTooltip effect="solid" getContent={this.copyMessage} />
             </div>
         )
     }
 
+    private copyMessage(): string {
+        return this.state.url === window.location.href ? '¡Copiado!' : 'Click para copiar enlace';
+    }
+
     private tweetMessage(): string {
-        const url = window.location.href;
-        const titles = this.props.series.map(serie => serie.title).join(' | ');
+        const url = window.location.href.replace('#', '%23').replace(/&/g, '%26');
+        const titles = this.props.series.map(serie => serie.title).join(' - ');
 
         return `http://twitter.com/intent/tweet?text=${titles} ${url}`;
     }
