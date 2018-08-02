8a051359921a489b5ebbe9f593ed1c40ac0c4a1b
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-08-02 14:30:58 -0300

    Fix panel derecho
        refs:#95

--- src/components/viewpage/DetallePanel.tsx
+++ src/components/viewpage/DetallePanel.tsx
@@ -9,7 +9,7 @@ export default class DetallePanel extends React.Component<IDetallePanelProps, {}
 
     public render() {
         return (
-            <div id="detalle-panel" style={{ height: "14792px" }}>
+            <div id="detalle-panel">
                 <div className="dp-header">
                     <h3 className="title-xsm font-1">AGREGAR SERIES</h3>
                     <button className="btn btn-link dp-header-close">
@@ -17,13 +17,11 @@ export default class DetallePanel extends React.Component<IDetallePanelProps, {}
                     </button>
                 </div>
                 <div className="dp-body">
-
                     <ul className="nav nav-tabs nav-justified">
                         <li className="active"><a href="#tab1" data-toggle="tab">Agregar Serie</a></li>
                     </ul>
 
-                    <div className="tab-content" style={{ height: '1173px' }}>
-
+                    <div className="tab-content">
                         <div className="tab-pane fade in active" id="tab1">
                             {this.props.seriesPicker}
                         </div>
