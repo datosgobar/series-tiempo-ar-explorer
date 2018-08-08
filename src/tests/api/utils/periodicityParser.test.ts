4cff48db052bcef14d5897f0da01e612da551320
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-08-08 09:41:31 -0300

    Agrego tests de periodicity parser
        refs:#88

---
+++ src/tests/api/utils/periodicityParser.test.ts
@@ -0,0 +1,67 @@
+import {PeriodicityParser} from "../../../api/utils/periodicityParser";
+
+
+describe("PeriodicityParser", () => {
+    let periodicityParser: PeriodicityParser;
+
+
+    describe("Diaria", () => {
+        beforeEach(() => { periodicityParser = new PeriodicityParser('R/P1D'); });
+
+        it("returns Diaria", () => {
+            expect(periodicityParser.formattedPeriodicity()).toEqual('Diaria');
+        });
+
+        it("returns 2018-05-16", () => {
+            expect(periodicityParser.formatDate('2018-05-16')).toEqual('2018-05-16');
+        });
+    });
+
+    describe("Mensual", () => {
+        beforeEach(() => { periodicityParser = new PeriodicityParser('R/P1M'); });
+
+        it("returns Mensual", () => {
+            expect(periodicityParser.formattedPeriodicity()).toEqual('Mensual');
+        });
+
+        it("returns 2018-05", () => {
+            expect(periodicityParser.formatDate('2018-05-16')).toEqual('2018-05');
+        });
+    });
+
+    describe("Anual", () => {
+        beforeEach(() => { periodicityParser = new PeriodicityParser('R/P1Y'); });
+
+        it("returns Anual", () => {
+            expect(periodicityParser.formattedPeriodicity()).toEqual('Anual');
+        });
+
+        it("returns 2018", () => {
+            expect(periodicityParser.formatDate('2018-05-16')).toEqual('2018');
+        });
+    });
+
+    describe("Trimestral", () => {
+        beforeEach(() => { periodicityParser = new PeriodicityParser('R/P3M'); });
+
+        it("returns Trimestral", () => {
+            expect(periodicityParser.formattedPeriodicity()).toEqual('Trimestral');
+        });
+
+        it("returns 2018-05", () => {
+            expect(periodicityParser.formatDate('2018-05-16')).toEqual('2018-05');
+        });
+    });
+
+    describe("Semestral", () => {
+        beforeEach(() => { periodicityParser = new PeriodicityParser('R/P6M'); });
+
+        it("returns Semestral", () => {
+            expect(periodicityParser.formattedPeriodicity()).toEqual('Semestral');
+        });
+
+        it("returns 2018-05", () => {
+            expect(periodicityParser.formatDate('2018-05-16')).toEqual('2018-05');
+        });
+    });
+});
