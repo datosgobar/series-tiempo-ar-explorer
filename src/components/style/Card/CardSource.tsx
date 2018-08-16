f890780dc5d6279eeead593607c29141362be764
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-08-16 10:27:30 -0300

    Corrijo estilo en las tarjetas y agrego archivos .css
        refs:#158

--- src/components/style/Card/CardSource.tsx
+++ src/components/style/Card/CardSource.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 
 export default (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>) =>
 
-    <span className="card-source" {...props}/>
+    <span className="card-source v-clamp-2" {...props}/>
