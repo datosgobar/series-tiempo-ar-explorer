f43031587c98b0fde39151add04e99bc8cd1af9d
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-08-28 16:03:21 -0300

    Borro métodos que no se usan

--- src/components/viewpage/graphic/GraphicAndShare.tsx
+++ src/components/viewpage/graphic/GraphicAndShare.tsx
@@ -1,5 +1,6 @@
 import * as moment from 'moment';
 import * as React from 'react';
+import {IDataPoint} from "../../../api/DataPoint";
 import { IDateRange } from "../../../api/DateSerie";
 import { ISerie } from "../../../api/Serie";
 import { Color } from "../../style/Colors/Color";
@@ -54,45 +55,27 @@ export default class GraphicAndShare extends React.Component<IGraphicAndSharePro
     public chartExtremes(): {min: number, max: number} {
         if (this.props.series.length === 0) {return {min: 0, max: 0}}
 
-        const minDataIndex = this.props.series[0].data.findIndex((data) => data.date >= this.props.date.start);
-        let maxDataIndex = this.props.series[0].data.findIndex((data) => data.date >= this.props.date.end);
-        if (maxDataIndex === 0) { maxDataIndex = this.props.series[0].data.length - 1}
+        const minDataIndex = this.firstSerieData().findIndex((data) => data.date >= this.props.date.start);
+        let maxDataIndex = this.firstSerieData().findIndex((data) => data.date >= this.props.date.end);
+        if (maxDataIndex === 0) { maxDataIndex = this.firstSerieData().length - 1}
 
-        const min = new Date(this.props.series[0].data[minDataIndex].date).getTime();
-        const max = new Date(this.props.series[0].data[maxDataIndex].date).getTime();
+        const min = new Date(this.firstSerieData()[minDataIndex].date).getTime();
+        const max = new Date(this.firstSerieData()[maxDataIndex].date).getTime();
 
         return {min, max};
     }
 
-    public startDate(): string {
-        return this.getSelectedDate(this.props.date.start, () => this.firstDateOfSerie());
-    }
-
-    public endDate(): string {
-        return this.getSelectedDate(this.props.date.end, () => this.lastDateOfSerie());
-    }
-
-    private getSelectedDate(selected: string, getDefault: () => string): string {
-       let result = selected;
-
-        if ((emptyValue(result)) && this.props.series.length > 0) {
-            result = getDefault();
-        }
-
-        return formattedDateString(result);
-    }
-
     private findSerieDate(value: number): string {
-        const serieData = this.props.series[0].data.find((data) => data.date >= timestampToDateString(value));
+        const serieData = this.firstSerieData().find((data) => data.date >= formattedMoment(new Date(value)));
         return serieData !== undefined ? serieData.date : '';
     }
 
-    private firstSerie(): ISerie {
-        return this.props.series[0];
+    private firstSerieData(): IDataPoint[] {
+        return this.props.series[0].data;
     }
 
     private firstDateOfSerie(): string {
-        return this.firstSerie().data[0].date;
+        return this.firstSerieData()[0].date;
     }
 
     private lastDateOfSerie(): string {
@@ -103,21 +86,8 @@ export default class GraphicAndShare extends React.Component<IGraphicAndSharePro
 }
 
 
-// returns a string in format YYYY/MM/DD with the missing parts of date
-// '2010' => '2010/01/01
-// '2010-03' or '2010/03' => '2010/03/01'
-// '2010-03-01' or '2010/03/01' => '2010/03/01'
-function formattedDateString(date: string): string {
-    const parsedDate  = date.replace(/([\/\-])/g, '-');
-    return parsedDate.split('-').length === 1 ? `${parsedDate}-01` : parsedDate;
-}
-
 function emptyValue(value: any): boolean {
     return value === '' || value === null || value === undefined
-}
-
-function timestampToDateString(value: any): string {
-    return formattedMoment(new Date(value));
 }
 
 function formattedMoment(date: any): string {
