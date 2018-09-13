beadf7f9e91738e0991ec629c41fff1dda4bbddc
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-09-13 09:16:33 -0300

    Agrego fonts al release

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,4 +12,8 @@ cp -r build/assets/images dist/
 cp -r build/static/css/main.*.css dist/css/main.css
 cp -r build/static/js/main.*.js dist/js/main.js
 
+mkdir -p dist/fonts/roboto dist/fonts/gotham
+cp -r build/assets/fonts/roboto/* dist/fonts/roboto/
+cp -r build/assets/fonts/gotham/* dist/fonts/gotham/
+
 echo "Release finished successfully!"
