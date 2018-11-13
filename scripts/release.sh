e6d9f73ec04738cdae9408f72536708cbf613881
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-11-13 10:56:06 -0300

    Agrego Graphic al proceso de release
        refs:#220

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,6 +11,8 @@ cp -r build/assets/ico dist/
 cp -r build/assets/images dist/
 cp -r build/assets/css/main.css dist/css/main.css
 cp -r build/static/js/main.*.js dist/js/main.js
+cp -r build/static/js/graphic.*.js dist/js/graphic.js
+rm build/static/js/graphic.*.js
 
 mkdir -p dist/fonts/roboto dist/fonts/gotham
 cp -r build/assets/fonts/roboto/* dist/fonts/roboto/
