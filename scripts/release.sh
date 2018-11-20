a1aa7d493b3b56de293afc0d32b0e9605f531163
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-11-20 14:49:46 -0300

    Borro instrucción duplicada de release.sh
        refs:#220

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,8 +14,6 @@ cp -r build/static/js/main.*.js dist/js/main.js
 cp -r build/static/js/graphic.*.js dist/js/graphic.js
 rm build/static/js/graphic.*.js
 
-cp -r build/static/js/graphic.*.js dist/js/graphic.js
-
 mkdir -p dist/fonts/roboto dist/fonts/gotham
 cp -r build/assets/fonts/roboto/* dist/fonts/roboto/
 cp -r build/assets/fonts/gotham/* dist/fonts/gotham/
