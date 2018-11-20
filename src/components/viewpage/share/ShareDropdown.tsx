350f94c0223a6856c5a36888b257a3bd6afc1c26
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-11-20 14:12:22 -0300

    Agrego enlace para copiar el código web
        refs:#220

--- src/components/viewpage/share/ShareDropdown.tsx
+++ src/components/viewpage/share/ShareDropdown.tsx
@@ -13,6 +13,7 @@ export default (props: IShareDropdownProps) =>
         <LinkShareItem url={webShareURL(props.url)} text="Enlace web" />
         <LinkShareItem url={csvShareURL(props.url)} text="Enlace CSV" />
         <LinkShareItem url={jsonShareURL(props.url)} text="Enlace JSON" />
+        <LinkShareItem url={webCodeUrl()} text="Código web" />
     </ShareDropdownContainer>
 
 
@@ -39,4 +40,23 @@ function urlToString(url: string): string {
               .replace(new RegExp('%3A', 'g'), ':')
               .replace(new RegExp('%2F', 'g'), '/')
               .replace(new RegExp('%3F', 'g'), '?');
+}
+
+function webCodeUrl(): string {
+    return "<script type='javascript' src='https://cdn.jsdelivr.net/gh/datosgobar/series-tiempo-ar-explorer@graphic/dist/js/graphic.js'/>\n" +
+        "<div id=\"root\"></div>\n" +
+        "<script>\n" +
+        "    window.onload = function() {\n" +
+        "        TSExplorer.render('root', {\n" +
+        "            serieIds: [\n" +
+        "                \"103.1_I2N_2016_M_15\",\n" +
+        "                \"168.1_T_CAMBIOR_D_0_0_26\",\n" +
+        "                \"308.1_PIB_IMPORTNES_0_T_17\"\n" +
+        "            ],\n" +
+        "            seriesApiUri: \"https://apis-stg.datos.gob.ar/series/api\",\n" +
+        "            chartOptions: { // Override highstock configs. See https://api.highcharts.com/highstock/\n" +
+        "            }\n" +
+        "        })\n" +
+        "    }\n" +
+        "</script>\n"
 }
