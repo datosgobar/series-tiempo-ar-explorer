fc358431a5ba649a56a190b6545c1205c0191a92
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-11-28 13:17:01 -0300

    Nuevos estilos en la tarjeta de series
        refs:#258

--- src/components/style/Card/CardTitle.tsx
+++ src/components/style/Card/CardTitle.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 
 export default (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) =>
 
-    <h3 className="card-title" {...props}/>
+    <h3 className="card-title v-clamp-2" {...props}/>
