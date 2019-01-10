d25cf46548fa68961e3999defd5ae1ad3ca658fd
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-01-10 09:49:31 -0300

    El release de TSComponents genera components.css
        refs:#279

--- scripts/release-components.sh
+++ scripts/release-components.sh
@@ -6,5 +6,6 @@ npm run build
 echo "Exporting components..."
 
 cp -r build/static/js/components.*.js dist/js/components.js
+cp -r build/assets/css/components.css dist/css/components.css
 
 echo "Process finished successfully!"
