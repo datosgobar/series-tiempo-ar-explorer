158294599f490eb7507f8987b389c1a04a9263f2
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-01-10 11:50:44 -0300

    Agrego TSComponents v1.5.4

--- src/components/viewpage/share/ShareDropdown.tsx
+++ src/components/viewpage/share/ShareDropdown.tsx
@@ -41,7 +41,7 @@ function urlToString(url: string): string {
 }
 
 function webCodeUrl(url: string): string {
-    return "<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/datosgobar/series-tiempo-ar-explorer@ts_components_1.5.1/dist/js/components.js'></script>\n" +
+    return "<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/datosgobar/series-tiempo-ar-explorer@ts_components_1.5.4/dist/js/components.js'></script>\n" +
         "<script type='text/css' src='https://cdn.jsdelivr.net/gh/datosgobar/series-tiempo-ar-explorer@ts_components_1.5.1/dist/css/components.css'></script>\n" +
         "<div id=\"root\"></div>\n" +
         "<script>\n" +
