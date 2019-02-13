ae353ca5edb16744e21d1156baa201af4b1ccd74
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-02-13 14:31:59 -0300

    Fix orden de colores

--- src/components/style/Colors/Color.ts
+++ src/components/style/Colors/Color.ts
@@ -2,16 +2,17 @@ export class Color {
     constructor(public name: string, public code: string){}
 }
 
+// "a<number>" is because order is important
 const Colors = {
-    Blue1: new Color("blue1", "#0072BB"),
-    Blue2: new Color("blue2", "#039BE5"),
-    Green1: new Color("green1","#2E7D33"),
-    Green2: new Color("green2", "#6EA100"),
-    Orange: new Color("orange", "#F9A822"),
-    Pink: new Color("pink", "#EC407A"),
-    Purple: new Color("purple", "#C2185B"),
-    Red: new Color("red", "#C62828"),
-    Violet: new Color("violet", "#6A1B99"),
+    a1Blue1: new Color("blue1", "#0072BB"),
+    a2Green1: new Color("green1","#2E7D33"),
+    a3Red: new Color("red", "#C62828"),
+    a4Orange: new Color("orange", "#F9A822"),
+    a5Violet: new Color("violet", "#6A1B99"),
+    a6Pink: new Color("pink", "#EC407A"),
+    a7Purple: new Color("purple", "#C2185B"),
+    a8Blue2: new Color("blue2", "#039BE5"),
+    a9Green2: new Color("green2", "#6EA100"),
 };
 export const NaC = new Color("", "");
 
