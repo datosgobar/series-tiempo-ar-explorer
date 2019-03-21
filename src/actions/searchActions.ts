f5e48f0f68ad39a5b333a2e485cbd4199e7edc78
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-03-21 15:01:31 -0300

    Guardo en el Store las aggregations que manda la API
        refs:#66

--- src/actions/searchActions.ts
+++ src/actions/searchActions.ts
@@ -1,11 +1,15 @@
 import { Action } from "redux";
-
+import { IAggregations } from "../api/ITSAPIResponse";
 import { ISearchParams } from "../components/common/searcher/Searcher";
 import actionTypes from "./actionTypes";
 
 
 export interface ISearchAction extends Action<string> {
     searchParams: ISearchParams
+}
+
+export interface ISetAggsAction extends Action<string> {
+    aggregations: IAggregations;
 }
 
 export function setSearchParams(searchParams:ISearchParams): ISearchAction {
@@ -15,4 +19,8 @@ export function setSearchParams(searchParams:ISearchParams): ISearchAction {
             type: actionTypes.SET_SEARCH_PARAMS,
         }
     );
+}
+
+export function setSourceFilters(aggregations: IAggregations): ISetAggsAction {
+    return ({ aggregations, type: actionTypes.SET_AGGREGATIONS })
 }
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,16 +1,18 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import searchParams from '../components/common/searcher/SearcherReducer';
 import featured from '../components/mainpage/featured/FeaturedReducer';
+import aggregations from '../components/searchpage/AggregationsReducer';
 import date from "../components/viewpage/DateReducer";
 import serieTags from "../components/viewpage/SerieTagsReducer";
 import viewSeries from '../components/viewpage/ViewPageReducer';
 import formatUnits from "../FormatUnitsReducer";
-import laps from '../lapsReducer'
-import locale from '../localeReducer'
+import laps from '../lapsReducer';
+import locale from '../localeReducer';
 import seriesApi from '../SerieApiReducer';
 
 
 const rootReducer = combineReducers({
+    aggregations,
     date,
     featured,
     formatUnits,
