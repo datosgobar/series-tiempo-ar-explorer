c082b2609fedb3d738da5b0b4d413fa72d949dd9
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-04-01 09:49:23 -0300

    Defino URLSearchParams si no existe, para ie11

--- config/polyfills.js
+++ config/polyfills.js
@@ -176,3 +176,20 @@ if (!Array.from) {
     };
   }());
 }
+
+// Fix for ie11: 'URLSearchParams' is undefined
+(function (w) {
+  w.URLSearchParams = w.URLSearchParams || function (searchString) {
+      var self = this;
+      self.searchString = searchString;
+      self.get = function (name) {
+          var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(self.searchString);
+          if (results == null) {
+              return null;
+          }
+          else {
+              return decodeURI(results[1]) || 0;
+          }
+      };
+  }
+})(window)
