742b946893632a963b849a9233d05a7759765d1e
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-04-11 14:58:47 -0300

    Agrego test de GraphicExportable

---
+++ src/tests/components/exportable/GraphicExportable.test.tsx
@@ -0,0 +1,18 @@
+import { configure, mount } from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+import * as React from 'react';
+import GraphicExportable from '../../../components/exportable/GraphicExportable';
+
+
+configure({ adapter: new Adapter() });
+
+describe('GraphicExportable', () => {
+    it('renders without crashing', () => {
+        const wrapper = mount(<GraphicExportable graphicUrl="https://apis.datos.gob.ar/series/api/series/?metadata=full&ids=116.3_TCRC_0_M_22&last=24"
+                                                    chartOptions={{}}
+                                                    chartTypes={{}} />);
+
+        expect(wrapper.find(GraphicExportable).exists()).toBe(true);
+    });
+
+})
