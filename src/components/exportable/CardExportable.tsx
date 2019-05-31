b86a59da5851ab471d3c391770188772cf189218
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-05-31 11:30:49 -0300

    Agrego formateo y serie modificada
        refs:#371

--- src/components/exportable/CardExportable.tsx
+++ src/components/exportable/CardExportable.tsx
@@ -3,7 +3,7 @@ import { ApiClient } from '../../api/ApiClient';
 import QueryParams from '../../api/QueryParams';
 import { ISerie } from '../../api/Serie';
 import SerieApi from '../../api/SerieApi';
-import { shortLocaleDate } from '../../helpers/dateFunctions';
+import { fullLocaleDate } from '../../helpers/dateFunctions';
 import { buildLocale } from '../common/locale/buildLocale';
 
 
@@ -20,6 +20,7 @@ export interface ICardExportableProps {
 
 interface ICardExportableState {
     serie: ISerie|null;
+    seriePercentChange: ISerie|null;
 }
 
 export default class CardExportable extends React.Component<ICardExportableProps, ICardExportableState> {
@@ -30,25 +31,28 @@ export default class CardExportable extends React.Component<ICardExportableProps
 
         this.seriesApi = new SerieApi(new ApiClient(buildURLfromSerieId(props.serieId), 'ts-components-card'));
         this.state = {
-            serie: null
+            serie: null,
+            seriePercentChange: null
         }
     }
 
     public componentDidMount() {
-        const params = new QueryParams([this.props.serieId]);
+        const percentChangeId = `${this.props.serieId}:percent_change_a_year_ago`
+        const params = new QueryParams([this.props.serieId, percentChangeId]);
+        params.setLast(1);
         this.fetchSeries(params);
     }
 
     public render() {
-        if (this.state.serie) {
+        if (this.state.serie && this.state.seriePercentChange) {
             return (
                 <div>
                     <strong>{this.state.serie.description}</strong>
-                    <h5>{shortLocaleDate(this.state.serie.accrualPeriodicity, this.state.serie.data[this.state.serie.data.length - 1].date)}</h5>
-                    <h1>{buildLocale(this.props.locale).toDecimalString(this.state.serie.data[this.state.serie.data.length - 1].value)}</h1>
-                    <h5>{this.state.serie.datasetTitle}</h5>
-                    <h3>-1,19% interanual</h3>
-                    <h5>{this.state.serie.datasetSource}</h5>
+                    <h5>{fullLocaleDate('Diaria', this.state.serie.data[0].date)}</h5>
+                    <h1>{buildLocale(this.props.locale).toDecimalString(this.state.serie.data[0].value)}</h1>
+                    <h5>{this.state.serie.units}</h5>
+                    <h3>{buildLocale(this.props.locale).toDecimalString(this.state.seriePercentChange.data[0].value)}% interanual</h3>
+                    <h5>Fuente: {this.state.serie.datasetSource}</h5>
                 </div>
             )
         }
@@ -58,7 +62,7 @@ export default class CardExportable extends React.Component<ICardExportableProps
 
     private fetchSeries(params: QueryParams) {
         this.seriesApi.fetchSeries(params)
-            .then((series: ISerie[]) => this.setState({serie: series[0]}))
+            .then((series: ISerie[]) => this.setState({serie: series[0], seriePercentChange: series[1]}))
             .catch((error: any) => alert("Ocurrió un error buscando la serie solicitada."));
     }
 }
