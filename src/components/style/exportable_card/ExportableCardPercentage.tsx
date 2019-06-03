1e664ea3e872a209acf6f18976e1d20c2e4711ac
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-06-03 10:01:42 -0300

    Borro componente que no se usa
        refs:#371

--- src/components/style/exportable_card/ExportableCardPercentage.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-
-export default (props: {color: string, text: string}) =>
-    <span className="exportable-card-percentage" style={{color: props.color}}>{props.text}</span>
