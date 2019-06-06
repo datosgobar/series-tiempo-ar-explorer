24f1c53d2d36037822325bb28079f8d8953f956f
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-06-06 15:02:54 -0300

    Fake CardFullChart porque ahora no se implementa
        refs:#373

--- src/components/exportable_card/charts/CardFullChart.tsx
+++ src/components/exportable_card/charts/CardFullChart.tsx
@@ -1,22 +1,16 @@
 import * as React from 'react';
-import D3LineChart from '../../d3_charts/D3LineChart';
 import { ICardChartProps } from '../FullCardChart';
+import CardSmallChart from './CardSmallChart';
 
 export default class CardFullChart extends React.Component<ICardChartProps, any> {
 
-    private readonly myRef: React.RefObject<any>;
-
     public constructor(props: ICardChartProps) {
         super(props);
-
-        this.myRef = React.createRef();
     }
 
     public render() {
         return (
-            <div ref={this.myRef}>
-                <D3LineChart renderTo={this.myRef} data={this.props.data} />
-            </div>
+            <CardSmallChart data={this.props.data} />
         )
     }
 }
