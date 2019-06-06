9f90f8879bc35309fc68e80c77d7924624728df3
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-06-06 15:01:23 -0300

    Quito nulos del mini grafico
        refs:#373

--- src/components/exportable_card/charts/CardSmallChart.tsx
+++ src/components/exportable_card/charts/CardSmallChart.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import { IDataPoint } from '../../../api/DataPoint';
 import D3LineChart from '../../d3_charts/D3LineChart';
 import { ICardChartProps } from '../FullCardChart';
+
 
 export default class CardSmallChart extends React.Component<ICardChartProps, any> {
 
@@ -15,8 +17,12 @@ export default class CardSmallChart extends React.Component<ICardChartProps, any
     public render() {
         return (
             <div ref={this.myRef}>
-                <D3LineChart renderTo={this.myRef} data={this.props.data} />
+                <D3LineChart renderTo={this.myRef} data={notNullData(this.props.data)} />
             </div>
         )
     }
+}
+
+function notNullData(data: IDataPoint[]): IDataPoint[] {
+    return data.filter((d: IDataPoint) => d.value !== null);
 }
