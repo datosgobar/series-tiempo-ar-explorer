03b7cc817399b7bb41eb346adea74becd5a445ab
pepeciavirella <pep.ciavirella@gmail.com>
2019-06-18 14:56:36 -0300

    Agrego estilos para fijar altura de las partes de la Card, e implemento Shiitake para clampear

--- src/components/style/exportable_card/FullCardSource.tsx
+++ src/components/style/exportable_card/FullCardSource.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-
+import Shiitake from 'shiitake'
 
 export default (props: {source: string, override: string}) => {
     if (props.override === "") {
         return <div/>
     }
     const source = props.override === undefined ? "Fuente: " + props.source : props.override
-    return <p className="c-span bt c-source">{source}</p>
+    return <Shiitake lines={2} className="c-span bt c-source" tagName="p">{source}</Shiitake>
 }
