cb2ee05d65467f56092244c3e72bf7517c88908d
Alexis Wolfsdorf <awolfsdorf@devartis.com>
2019-08-27 13:03:21 -0300

    Refactor para que no haya lineas duplicadas

--- src/tests/components/viewpage/graphic/hcSerieFromISerie.test.ts
+++ src/tests/components/viewpage/graphic/hcSerieFromISerie.test.ts
@@ -16,6 +16,22 @@ describe("High chart Series from ISerie functions", () => {
     const locale = 'AR';
     const formatUnits = false;
 
+    function getIHcSeriesBySeries(series: ISerie[]) : IHCSeries[] {
+        const yAxisBySeries = generateYAxisBySeries(series, mockConfig, formatUnits, locale);
+        const yAxisArray = generateYAxisArray(yAxisBySeries);
+        const options: IHighchartsSerieBuilderOptions = {
+            chartTypes: { "Motos_patentamiento_8myrF9": "column"},
+            colors: undefined,
+            legendLabel: undefined,
+            legendField: undefined,
+            series,
+            yAxisBySeries,
+            yAxisArray
+        }
+        return series.map((serie) => new HighchartsSerieBuilder(options)
+                                            .buildFromSerie(serie));
+    }
+
     beforeAll(() => {
         mockSerieOne = generateCommonMockSerieEMAE();
         mockSerieTwo = generateCommonMockSerieMotos();
@@ -28,21 +44,8 @@ describe("High chart Series from ISerie functions", () => {
 
         beforeAll(() => {
             const series: ISerie[] = [mockSerieOne, mockSerieTwo];
-            const yAxisBySeries = generateYAxisBySeries(series, mockConfig, formatUnits, locale);
-            const yAxisArray = generateYAxisArray(yAxisBySeries);
-            const options: IHighchartsSerieBuilderOptions = {
-                chartTypes: { "Motos_patentamiento_8myrF9": "column"},
-                colors: undefined,
-                legendLabel: undefined,
-                legendField: undefined,
-                series,
-                yAxisBySeries,
-                yAxisArray
-            }
-            iHcSeries = series.map((serie) => new HighchartsSerieBuilder(options)
-                                                    .buildFromSerie(serie));
+            iHcSeries = getIHcSeriesBySeries(series);
         })
-
         it("Cada serie tiene su nombre y serie correspondiente", () => {
             expect(iHcSeries[0].serieId).toEqual("EMAE2004");
             expect(iHcSeries[0].name).toEqual("EMAE. Base 2004 (izq)");
@@ -70,43 +73,30 @@ describe("High chart Series from ISerie functions", () => {
 
         beforeAll(() => {
             const series: ISerie[] = [mockSerieOne, mockSerieTwo, mockSerieThree];
-            const yAxisBySeries = generateYAxisBySeries(series, mockConfig, formatUnits, locale);
-            const yAxisArray = generateYAxisArray(yAxisBySeries);
-            const options: IHighchartsSerieBuilderOptions = {
-                chartTypes: { "Motos_patentamiento_8myrF9": "column"},
-                colors: undefined,
-                legendLabel: undefined,
-                legendField: undefined,
-                series,
-                yAxisBySeries,
-                yAxisArray
-            }
-            iHcSeries = series.map((serie) => new HighchartsSerieBuilder(options)
-                                                    .buildFromSerie(serie));
+            iHcSeries = getIHcSeriesBySeries(series);
         })
-
         it("Cada serie tiene su nombre y serie correspondiente", () => {
-            expect(iHcSeries[0].serieId).toEqual("EMAE2004");
-            expect(iHcSeries[0].name).toEqual("EMAE. Base 2004 (izq)");
-            expect(iHcSeries[1].serieId).toEqual("Motos_patentamiento_8myrF9");
-            expect(iHcSeries[1].name).toEqual("Motos: número de patentamientos de motocicletas (der)");
             expect(iHcSeries[2].serieId).toEqual("EMAE2004:percent_change_a_year_ago");
             expect(iHcSeries[2].name).toEqual("EMAE. Base 2004 (izq)");
+            expect(iHcSeries[1].serieId).toEqual("Motos_patentamiento_8myrF9");
+            expect(iHcSeries[1].name).toEqual("Motos: número de patentamientos de motocicletas (der)");
+            expect(iHcSeries[0].serieId).toEqual("EMAE2004");
+            expect(iHcSeries[0].name).toEqual("EMAE. Base 2004 (izq)");
         });
         it("Cada serie tiene su chartType correspondiente", () => {
+            expect(iHcSeries[2].type).toEqual(undefined);
             expect(iHcSeries[0].type).toEqual(undefined);
             expect(iHcSeries[1].type).toEqual("column");
-            expect(iHcSeries[2].type).toEqual(undefined);
         });
         it("Cada serie tiene su yAxis correspondiente", () => {
-            expect(iHcSeries[0].yAxis).toEqual(0);
             expect(iHcSeries[1].yAxis).toEqual(1);
+            expect(iHcSeries[0].yAxis).toEqual(0);
             expect(iHcSeries[2].yAxis).toEqual(0);
         });
         it("Cada serie tiene su color correspondiente", () => {
             const colorArray = (Object as any).values(Colors);
-            expect(iHcSeries[0].color).toEqual(colorArray[0].code);
             expect(iHcSeries[1].color).toEqual(colorArray[1].code);
+            expect(iHcSeries[0].color).toEqual(colorArray[0].code);
             expect(iHcSeries[2].color).toEqual(colorArray[2].code);
         });
 
