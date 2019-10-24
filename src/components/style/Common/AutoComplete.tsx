ff2c40f3f200f211c4d2d9a851669f5fd4559138
pepeciavirella <pep.ciavirella@gmail.com>
2019-10-24 14:58:17 -0300

    Revierto cambios sobre AutoComplete, y escondo div auxiliar al searchbar del panel lateral de ViewPage

--- src/components/style/Common/AutoComplete.tsx
+++ src/components/style/Common/AutoComplete.tsx
@@ -2,13 +2,6 @@ import * as AutoComplete from 'react-autocomplete';
 
 import * as React from 'react';
 
-const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
-    event.preventDefault();
-};
-
 export default (props: AutoComplete.Props) =>
 
-    <AutoComplete inputProps={{ className: "form-control",
-                                onFocus: (handleFocus) }}
-                  wrapperStyle={{}}
-                  {...props} />
+    <AutoComplete inputProps={{ className: "form-control" }} wrapperStyle={{}} {...props} />
