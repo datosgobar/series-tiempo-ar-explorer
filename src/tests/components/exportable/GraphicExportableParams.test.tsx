7c8027b901e162d9706f06f0fc6f1aa37c554b9e
Alexis Wolfsdorf <awolfsdorf@devartis.com>
2019-11-04 12:18:03 -0300

    un test mas testeando casos de parametro invalido

--- src/tests/components/exportable/GraphicExportableParams.test.tsx
+++ src/tests/components/exportable/GraphicExportableParams.test.tsx
@@ -1,7 +1,9 @@
-import { mount } from 'enzyme';
+import { configure, mount } from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
 import * as React from 'react';
 import GraphicExportable from '../../../components/exportable/GraphicExportable';
 
+configure({ adapter: new Adapter() });
 
 describe('GraphicExportable params passing', () => {
     it('startDate override API start_date', () => {
@@ -28,5 +30,18 @@ describe('GraphicExportable params passing', () => {
         const graphicState = wrapper.state();
         const graphicEndDate = graphicState.dateRange.end;
         expect(graphicEndDate).toEqual("2009-10-8");
+    });
+
+    it('malformed start or end override too', () => {
+        const wrapper = mount(<GraphicExportable graphicUrl="https://apis.datos.gob.ar/series/api/series/?metadata=full&ids=116.3_TCRC_0_M_22&last=24&end_date=2010-10-08"
+                                                 chartOptions={{}}
+                                                 legendLabel={{}}
+                                                 seriesAxis={{}}
+                                                 chartTypes={{}}
+                                                 decimalTooltips={{}}
+                                                 endDate={"20090-8"} />);
+        const graphicState = wrapper.state();
+        const graphicEndDate = graphicState.dateRange.end;
+        expect(graphicEndDate).toEqual("20090-8");
     });
 })
